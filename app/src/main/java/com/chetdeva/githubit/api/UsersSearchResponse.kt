071faaf85d7ce0cc7993ba42b5ec1b7284fd149c
Chetan Sachdeva <chet.deva@gmail.com>
2018-07-04 21:08:01 +0530

    refactor(*): add SerializableName to UsersSearchResponse

--- app/src/main/java/com/chetdeva/githubit/api/UsersSearchResponse.kt
+++ app/src/main/java/com/chetdeva/githubit/api/UsersSearchResponse.kt
@@ -1,11 +1,16 @@
 package com.chetdeva.githubit.api
 
+import com.google.gson.annotations.SerializedName
+
 /**
  * @author chetansachdeva
  */
 
 class UsersSearchResponse {
+    @SerializedName("total_count")
     var totalCount: Int? = null
+    @SerializedName("incomplete_results")
     var incompleteResults: Boolean? = null
+    @SerializedName("items")
     var items: List<Item>? = null
 }
